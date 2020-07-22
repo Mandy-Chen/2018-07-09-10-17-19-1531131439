ef7378d413f3c16596bfe88e02d5fd0a756bd917
MANDY CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENMA18@OOCL.COM>
2020-07-22 16:36:35 +0800

    [practice06][mandy chen][feature: Add Teacher Class]

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,29 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Integer klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+    public String introduce() {
+        if (klass==null){
+            return "My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.";
+        }else {
+            return "My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2.";
+        }
+    }
 }
