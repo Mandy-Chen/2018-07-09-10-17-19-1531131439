a6c7ffe44e1af2b66dc0c7d9be4b6cf0012cef7a
MANDY CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENMA18@OOCL.COM>
2020-07-22 15:23:00 +0800

    [practice01][mandy chen][feature:Add name and age in Person]

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,21 @@
 package practice01;
 
 public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name="Tom";
+        this.age=21;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is Tom. I am 21 years old.";
+    }
 }
