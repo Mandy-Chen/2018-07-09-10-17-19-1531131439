d2f1292724c9ae3a2ccecc894a18ec333dcbfb27
Mandy <chenjiafeng16@126.com>
2020-07-22 22:20:17 +0800

    [practice08|09][mandy chen][feature:together]

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,45 @@
 package practice09;
 
+
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class "+this.number;
+    }
+    public void assignLeader(Student student){
+        if(student.getKlass().getNumber()==this.getNumber()){
+            if(student!=null){
+                this.leader=student;
+            }
+        }
+        else {
+            System.out.println("It is not one of us.");
+        }
+
+    }
+    public void appendMember(Student student){
+
+    }
 }
--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -1,4 +1,57 @@
 package practice09;
 
+import java.util.Objects;
+
 public class Person {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+    public String introduce(){
+        return "My name is Tom. I am 21 years old.";
+    }
+
 }
