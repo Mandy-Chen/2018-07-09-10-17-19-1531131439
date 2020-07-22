747e0aeb309ee7209a6be60614e5d4752bc86e3f
MANDY CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENMA18@OOCL.COM>
2020-07-22 15:49:48 +0800

    [pactice03][mandy chen][feature: Add Worker Class]

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,46 @@
 package practice03;
 
-public class Student {
+public class Student extends Person {
+    private int klass;
+    private String name;
+    private int age;
+
+    public Student(String name, int age, int klass) {
+        super();
+        this.klass = klass;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public String introduce() {
+        return "I am a Student. I am at Class 2.";
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
