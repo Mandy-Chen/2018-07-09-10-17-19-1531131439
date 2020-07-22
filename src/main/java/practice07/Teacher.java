f88d01891dff5b402da0302659a12e2050be838d
Mandy <chenjiafeng16@126.com>
2020-07-22 20:45:43 +0800

    [practice07][mandy chen][feature : Add Klass Class]

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,39 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+    public String introduceWith(Student student){
+        if (this.klass.getNumber()==student.getKlass().getNumber()){
+            return super.introduce()+" I am a Teacher. I teach "+student.getName()+".";
+        }else {
+            return super.introduce()+" I am a Teacher. I don't teach "+student.getName()+".";
+        }
+
+
+    }
+    public String introduce(){
+        if(klass==null){
+            return super.introduce()+" I am a Teacher. I teach No Class.";
+        }else {
+            return super.introduce()+" I am a Teacher. I teach Class 2.";
+        }
+
+    }
 }
